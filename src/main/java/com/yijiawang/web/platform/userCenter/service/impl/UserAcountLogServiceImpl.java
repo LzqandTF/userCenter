4d03fb80ef58a6997f841022d4998db0bb07debd
xuyang <xyy.hit@126.com>
2016-05-30 13:42:40 +0800

    1.add user account log

--- src/main/java/com/yijiawang/web/platform/userCenter/service/impl/UserAcountLogServiceImpl.java
+++ src/main/java/com/yijiawang/web/platform/userCenter/service/impl/UserAcountLogServiceImpl.java
@@ -2,6 +2,7 @@ package com.yijiawang.web.platform.userCenter.service.impl;
 
 import com.yijiawang.web.platform.userCenter.service.UserAccountLogService;
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -12,6 +13,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by xy on 16/5/30.
  */
+@Service("userAccountLogService")
 public class UserAcountLogServiceImpl implements UserAccountLogService {
 
     private static ExecutorService logUserAcountPool = new ThreadPoolExecutor(30, Integer.MAX_VALUE, 0L, TimeUnit.MILLISECONDS, new SynchronousQueue<Runnable>(), new ThreadPoolExecutor.CallerRunsPolicy());
