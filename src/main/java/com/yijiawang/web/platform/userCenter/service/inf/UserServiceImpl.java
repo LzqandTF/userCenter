7090a3b289c83fd20458b08836aa0cd07988d96e
yuanxiaodong <yuanxiaodong@yijiapai.com>
2016-11-30 17:45:43 +0800

    众筹流水

--- src/main/java/com/yijiawang/web/platform/userCenter/service/inf/UserServiceImpl.java
+++ src/main/java/com/yijiawang/web/platform/userCenter/service/inf/UserServiceImpl.java
@@ -859,7 +859,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public AccountCheck getZcOrderPayAccountCheck(String orderId) {
-        return accountCheckMapper.getOrderPayAccountCheck(orderId);
+        return accountCheckMapper.getZcOrderPayAccountCheck(orderId);
     }
 
     @Override
